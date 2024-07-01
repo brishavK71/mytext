e7aac4559dd225e8fdc621d6ead2ac121669da09
Brishav Kuikel <brishavk71@gmail.com>
2024-07-01 14:43:52 +0545

    in textform.js /s error

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -45,7 +45,7 @@ export default function TextForm(props) {
         
         <div className="container">
             <h1>Your Text Summary</h1>
-            <p> {text.split("/\s+/").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p> {text.split("/+/").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
             <p> {0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read</p>
             <h2>Preview</h2>
             <p>{text}</p>
